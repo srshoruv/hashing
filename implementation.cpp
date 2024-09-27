2ebf585fbdd10ce03d2b3f4f3ad3c531877e9048
srshoruv <srshoruv2002@gmail.com>
2024-09-27 20:37:04 +0600

    added search and print function

--- implementation.cpp
+++ implementation.cpp
@@ -30,10 +30,10 @@ class HashTable {
         int idx = 0;
 
         for (int i=0; i<key.size(); i++){
-            idx = idx + (key[i]*key[i]) % totSize;
+            idx = (idx + (key[i]*key[i])) % totSize;
         }
 
-        return idx;
+        return idx%totSize;
     }
 
     void rehash() {
@@ -41,6 +41,7 @@ class HashTable {
         int oldSize = totSize;
 
         totSize = 2 * totSize;
+        currSize = 0;
         table = new Node*[totSize];
 
         for (int i=0; i<totSize; i++) {
@@ -77,10 +78,8 @@ public:
         int idx = HashFunction(key);
 
         Node* newNode = new Node(key,val);
-        Node* head = table[idx];
-
-        newNode->next = head;
-        head = newNode;
+        newNode->next = table[idx];
+        table[idx] = newNode;
 
         currSize++;
 
@@ -91,10 +90,58 @@ public:
         }
     }
 
+    bool exists(string key) {
+        int idx = HashFunction(key);
+
+        Node* temp = table[idx];
+        while (temp) {
+            if (temp->key == key) {
+                return true;
+            }
+            temp = temp->next;
+        }
+
+        return false;
+    }
+
+    int search(string key) {
+        int idx = HashFunction(key);
+
+        Node* temp = table[idx];
+        while (temp) {
+            if (temp->key == key) {
+                return temp->val;
+            }
+            temp = temp->next;
+        }
+
+        return -1;
+    }
+    
+    void print() {
+        for (int i = 0; i<totSize; i++) {
+            cout << "idx" << i << "->";
+
+            Node* temp = table[i];
+            while (temp) {
+                cout << "(" << temp->key << "," << temp->val << ") ->";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+    }
     
 };
 
 int main() {
-    HashTable ht (5);
+    HashTable ht(5);
+
+    ht.insert("Bangladesh", 160);
+    ht.insert("India", 150);
+    ht.insert("China", 300);
+    ht.insert("Nepal", 30);
+    ht.insert("Bhutan", 10);
+
+    ht.print();
     return 0;
 }
