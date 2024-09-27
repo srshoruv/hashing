af7b58065d2916984c9e7943e697e3e19ab9a40a
srshoruv <srshoruv2002@gmail.com>
2024-09-27 20:13:12 +0600

    rehashing

--- implementation.cpp
+++ implementation.cpp
@@ -13,12 +13,55 @@ public:
         this->key = key;
         this->val = val;
     }
+
+    ~Node() {
+        if (next) {
+            delete next;
+        }
+    }
 };
 
 class HashTable {
     int totSize;
     int currSize;
     Node** table;
+
+    int HashFunction(string key) {
+        int idx = 0;
+
+        for (int i=0; i<key.size(); i++){
+            idx = idx + (key[i]*key[i]) % totSize;
+        }
+
+        return idx;
+    }
+
+    void rehash() {
+        Node** oldTable = table;
+        int oldSize = totSize;
+
+        totSize = 2 * totSize;
+        table = new Node*[totSize];
+
+        for (int i=0; i<totSize; i++) {
+            table[i] = NULL;
+        }
+
+        //copy old values
+
+        for (int i=0; i<oldSize; i++) {
+            Node* temp = oldTable[i];
+            while (temp) {
+                insert(temp->key, temp->val);
+                temp = temp->next;
+            }
+
+            if (oldTable[i]) {
+                delete oldTable[i];
+            }
+        }
+    }
+
 public:
     HashTable(int size) {
         totSize = size;
@@ -28,6 +71,24 @@ public:
         for (int i=0; i<totSize; i++) {
         table[i] = NULL;
     }
+    }
+
+    void insert(string key, int val) {
+        int idx = HashFunction(key);
+
+        Node* newNode = new Node(key,val);
+        Node* head = table[idx];
+
+        newNode->next = head;
+        head = newNode;
+
+        currSize++;
+
+        double lamda = currSize / (double) totSize;
+
+        if (lamda > 1) {
+            rehash();
+        }
     }
 
     
